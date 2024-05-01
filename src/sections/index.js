92f4ceb413c82debebbdb3d7fc18330121799127
Ashish Kumar <118570586+ashishkrobo@users.noreply.github.com>
2024-05-02 03:26:37 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export{
+    Hero,
+    PopularProducts,
+    CustomerReview,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
